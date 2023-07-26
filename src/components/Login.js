404b039a20470de7a493c1867aba8140758f2aa8
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-07-26 21:55:05 +0300

    programmatic programming

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function Login({ setIsLoggedIn }) {
+function Login({ onLogin }) {
   const history = useHistory();
   const [formData, setFormData] = useState({
     username: "",
@@ -18,10 +18,18 @@ function Login({ setIsLoggedIn }) {
   function handleSubmit(e) {
     e.preventDefault();
 
-    setIsLoggedIn(true);
-
+    fetch("http://localhost:3001/login",{
+        method:"POST",
+        headers:{
+            "content-Type":"application/json",
+        },
+        body:JSON.stringify(formData)
+    })
+        .then((r)=>r.json())
+        .then((user)=>{
+            onLogin(user)
     // after logging the user in, redirect to the home page!
-    history.push("/");
+    history.push("/home");})
   }
 
   return (
